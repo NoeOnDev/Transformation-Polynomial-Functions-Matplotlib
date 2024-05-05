411102d5e6305f35268d5613c9ad1c580b095b18
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-05-05 01:30:56 -0600

    Add plot for function A in A.py

--- B.py
+++ B.py
@@ -1,4 +1,4 @@
-# La gráfica de la función f(x) = |1 - x^2| + 3
+# Description: Gráfica de la función f(x) = |1 - x^2| + 3
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,11 +11,15 @@ x = np.linspace(-3, 3, 100)
 y_a = [func_a(val) for val in x]
 
 plt.figure(figsize=(8, 6))
-plt.plot(x, y_a, linewidth=2)
+plt.plot(x, y_a, linewidth=2, color='black')
 plt.xlabel('x', fontsize=14)
 plt.ylabel('y', fontsize=14)
-plt.title('Gráfica del inciso A', fontsize=16)
+plt.title('Gráfica de la función f(x) = |1 - x^2| + 3', fontsize=16)
 plt.grid(True)
 plt.axhline(y=0, color='k', linestyle='--')
 plt.axvline(x=0, color='k', linestyle='--')
+
+plt.ylim(0, 10)
+plt.yticks(np.arange(0, 11, 1))
+
 plt.show()
