90b2e6bae915a146f4482f915e7176c89bfedefb
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-05-05 02:43:53 -0600

    feat: Refactor y-axis displacement code for better readability
    The code changes in `shift-y-axis-up.py` improve the readability of the y-axis displacement code. This commit message follows the established convention of using a prefix to indicate the type of change, in this case, "feat" for a new feature.

--- y-axis-displacement/shift-y-axis-up.py
+++ y-axis-displacement/shift-y-axis-up.py
@@ -16,7 +16,6 @@ plt.xlabel('', fontsize=14)
 plt.ylabel('', fontsize=14)
 plt.title('Gráfica de la función F(x) = x^2 + 2', fontsize=16)
 plt.grid(True)
-
 ax = plt.gca()
 ax.spines['left'].set_position('center')
 ax.spines['bottom'].set_position('center')
@@ -24,16 +23,12 @@ ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
-
 plt.xlim(-5, 5)
 plt.ylim(-5, 5)
-
 x_ticks = np.arange(-5, 6, 1)
 x_labels = [str(i) if i != 0 else '' for i in x_ticks]
 y_ticks = np.arange(-5, 6, 1)
 y_labels = [str(i) if i != 0 else '' for i in y_ticks]
-
 plt.xticks(x_ticks, x_labels)
 plt.yticks(y_ticks, y_labels)
-
 plt.show()
