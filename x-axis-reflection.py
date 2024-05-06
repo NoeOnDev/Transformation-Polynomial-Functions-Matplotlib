5a29f04d09c7862098d53280cc1d346461b08728
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-05-06 09:42:29 -0600

    Refactor code for better readability and axis positioning

--- x-axis-reflection.py
+++ x-axis-reflection.py
@@ -1,46 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def func1(x):
-    return x
+x = np.linspace(0.1, 2, 400)
 
-def func2(x):
-    return -x
+y1 = -np.abs(np.log(x))
+y2 = np.abs(np.log(x))
 
-functions = [func1, func2]
-colors = ['red', 'blue']
-labels = ['F(x) = x', 'F(x) = -x']
+plt.figure()
 
-x = np.linspace(-5, 5, 400)
+plt.plot(x, y1, label='F(x) = -|Ln(x)|')
+plt.plot(x, y2, label='F(x) = |Ln(x)|')
 
-plt.figure(figsize=(8, 6))
-
-for func, color, label in zip(functions, colors, labels):
-    y = func(x)
-    plt.plot(x, y, color=color, label=label)
-
-plt.xlabel('', fontsize=14)
-plt.ylabel('', fontsize=14)
-plt.title('Gráficas de las funciones', fontsize=16)
-plt.grid(True)
-
-ax = plt.gca()
-ax.spines['left'].set_position('center')
-ax.spines['bottom'].set_position('center')
-ax.spines['right'].set_position('center')
-ax.spines['top'].set_position('center')
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
-
-plt.xlim(-5, 5)
-plt.ylim(-5, 5)
-
-x_ticks = np.arange(-5, 6, 1)
-x_labels = [str(i) if i != 0 else '' for i in x_ticks]
-y_ticks = np.arange(-5, 6, 1)
-y_labels = [str(i) if i != 0 else '' for i in y_ticks]
-
-plt.xticks(x_ticks, x_labels)
-plt.yticks(y_ticks, y_labels)
 plt.legend()
 plt.show()
