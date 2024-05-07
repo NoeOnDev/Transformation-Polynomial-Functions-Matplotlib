66dd3a7053d2a875dc65dd94f7aae1648fefadb9
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-05-06 21:23:06 -0600

    Refactor code for better readability and axis positioning

--- x-axis-reflection.py
+++ x-axis-reflection.py
@@ -1,36 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-2*np.pi, 2*np.pi, 400)
+def f(x):
+    return -np.abs(np.log(x))
 
-y = np.sin(x)
-y_abs = np.abs(np.sin(x))
+def f_abs(x):
+    return np.abs(np.log(x))
 
-plt.figure(figsize=(12, 4))
+x = np.linspace(0.01, 5, 400)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='$F(x) = \sin(x)$', color='b')
-plt.title('Gráfica de $F(x) = \sin(x)$')
+y = f(x)
+y_abs = f_abs(x)
+
+plt.figure(figsize=(6, 4))
+
+plt.plot(x, y, label='$y = -| \ln(x) |$', color='b')
+plt.plot(x, y_abs, label='$y = | \ln(x) |$', color='r')
+
+plt.title('Gráficas de las funciones')
 plt.legend()
+
 plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
 plt.axvline(x=0, color='black', linestyle='-', linewidth=1) 
+
 plt.gca().spines['left'].set_position(('data', 0))
 plt.gca().spines['right'].set_color('none')
 plt.gca().spines['bottom'].set_position(('data', 0))
 plt.gca().spines['top'].set_color('none')
+
 plt.grid(True)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y_abs, label='$F(x) = |\sin(x)|$', color='r')
-plt.title('Gráfica de $F(x) = |\sin(x)|$')
-plt.legend()
-plt.axhline(y=0, color='black', linestyle='-', linewidth=1)
-plt.axvline(x=0, color='black', linestyle='-', linewidth=1) 
-plt.gca().spines['left'].set_position(('data', 0))
-plt.gca().spines['right'].set_color('none')
-plt.gca().spines['bottom'].set_position(('data', 0))
-plt.gca().spines['top'].set_color('none')
-plt.grid(True)
+plt.xlim([0, 5])
+plt.ylim([-4, 4])
 
 plt.tight_layout()
 
